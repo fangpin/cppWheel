67aa34972799cdb96c839ce7f3021e78784002f7
fangpin <fangpin1993@hotmail.com>
2017-07-09 20:26:41 +0800

    init commit, coding shortcut template

---
+++ programming-contest-challenge-book/chap2/base.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    freopen("in.txt", "r", stdin);
+
+    return 0;
+}
